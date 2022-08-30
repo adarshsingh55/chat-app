92645c7200b66e7e6167cd58de9bec42ef55f013
Adarsh singh <adarshsingh345125@gmail.com>
2022-08-30 20:56:44 +0530

    chat app

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("hello script");
+const socket =io('http://localhost:8000',{transports:['websocket']})
+const form =document.getElementById("sent-container")
+const messageInput = document.getElementById("message-inp")
+const messageContainer =document.querySelector('.container')
+
+const append=(message,position)=>{
+    const messageElement =document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.appendChild(messageElement);
+}
+
+const names = prompt("enter your name to join")
+socket.emit('new-user-joined', names )
+socket.on('user-joined',name=>{
+    console.log(name);
+    append(`${name} joined the chat`,"right");
+    
+})
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const message =messageInput.value;
+    append(`you : ${message}`,"right")
+    socket.emit('send',message)
+    messageInput.value =""
+})
+socket.on('receive',data=>{
+    append(`${data.name} : ${data.message}`,"left")
+})
+socket.on('leave',data=>{
+    append(`${data.name} left the chat`,"left")
+})
